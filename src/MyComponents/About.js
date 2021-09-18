a5b644461e6446332585c69f8055f0be42b98ade
HarshParmar25 <harshparmar2506@gmail.com>
2021-09-18 11:08:50 -0400

    init

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about componenet
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut labore
+        placeat quidem, porro quo necessitatibus reprehenderit eveniet debitis
+        officia, dolore perferendis illo voluptatibus nisi dicta, unde dolores.
+        Odit, tempora! Ipsa doloribus veritatis neque praesentium.
+      </p>
+    </div>
+  );
+};
